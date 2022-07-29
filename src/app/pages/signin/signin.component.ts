70fda38b560b71ab3ccf8ec248e7af218d76c5bb
naveen <naveennanigande422@gmail.com>
2022-07-29 11:19:55 +0530

    signin page validations

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+ userForm!: FormGroup;
+ email:string='';
+ password:any='';
+ public showPassword: boolean = false;
+ 
 
-  ngOnInit(): void {
+  constructor(public fb:FormBuilder) { }
+
+  ngOnInit() {
+  
+     this.userForm = this.fb.group ({
+      emailId: ['', Validators.required],
+      Password: ['', [Validators.required , Validators.minLength(4)]],
+    })
+  }
+  public togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
   }
 
+    onSubmit(){
+      if(this.userForm.invalid) {
+        this.userForm.markAsTouched();
+      }
+    
+      console.log(this.userForm.value);
+      
+    }
+  
+  
 }
