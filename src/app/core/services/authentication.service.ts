b0db410c89b431a8ff974e160c831af4308162f2
naveen <naveennanigande422@gmail.com>
2022-08-02 07:51:42 +0530

    Authatication service

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -8,10 +8,22 @@ import { DataClientService } from './data-client.service';
   providedIn: 'root'
 })
 export class AuthenticationService {
+  isLoggedIn = false;
 
   constructor(private dataClientService: DataClientService) { }
   // Service method to create a new user
   public signUp(userData:any): Observable<IUser> {
     return this.dataClientService.post<IUser>(HttpUrls.USER_CREATE_ACCOUNT, userData);
   }
+
+  public logIn(loginCred:any){
+    return this.dataClientService.post(HttpUrls.USER_LOGIN, loginCred)
+      // localStorage.setItem('access_token', res.accessToken);
+  }
+
+  public loggedIn(){
+    return this.isLoggedIn;
+
+  }
+
 }
